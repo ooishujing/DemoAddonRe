08af8cecb3c61dbb91d6a791dc43500c83d145c7
ooishujing <18016012@myrp.edu.sg>
2021-05-29 17:36:47 +0800

    Forget

---
+++ app/src/main/java/sg/edu/rp/c346/demoaddon/MainActivity.java
@@ -0,0 +1,25 @@
+package sg.edu.rp.c346.demoaddon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv = findViewById(R.id.iv);
+        String imageUrl = "https://i.imgur.com/tGbaZCY.jpg";
+        Picasso.with(this).load(imageUrl).into(iv);
+
+
+    }
+}
